6728a53708249473f49b1e91600bf1cbbdad9c0d
Jonathan Hart <stuajnht@gmail.com>
2017-02-16 16:50:20 +0000

    Created Init Test Class
    Created a class to test the Init functions, and included a test to see if the OS returned matches the currently running OS

---
+++ CSSSTests/InitTests.cs
@@ -0,0 +1,63 @@
+﻿//  CSSSTests - CyberSecurity Scoring System Tests
+//  Copyright(C) 2017  Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using CSSS;
+using NUnit.Framework;
+using System;
+
+namespace CSSSTests
+{
+    /// <summary>
+    /// Runs all tests related to the init class
+    /// </summary>
+    [TestFixture()]
+    public class InitTests
+    {
+        private Init initTests;
+
+        /// <summary>
+        /// Creates an instance of the init class
+        /// </summary>
+        [SetUp]
+        protected void SetUp()
+        {
+            initTests = new Init();
+        }
+
+        /// <summary>
+        /// Removes any reference to the init class
+        /// </summary>
+        [TearDown]
+        protected void TearDown()
+        {
+            initTests = null;
+        }
+
+        /// <summary>
+        /// Sees if the Operating System type returned matches
+        /// one of the supported ones in CSSS. This test is dependent
+        /// on the current Operating System running on the computer
+        /// running the tests
+        /// </summary>
+        [Test()]
+        public void TestOperatingSystemDetected()
+        {
+            Assert.AreNotEqual(Config.CurrentOperatingSystem.Unknown,
+                               initTests.SetCurrentOperatingSystem(),
+                               "The operating system detected should not be an unknown type");
+        }
+    }
+}
