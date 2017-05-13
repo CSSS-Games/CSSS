0c05a18b073c18500a9ad961bcd7ee5335976207
Jonathan Hart <stuajnht@gmail.com>
2017-05-13 20:16:02 +0100

    Created Prepare.cs Class
    The prepare class is used by the Kernel when the "--prepare" argument is passed to CSSS, to perform various changes to get CSSS ready for image capture

---
+++ CSSS/Prepare.cs
@@ -0,0 +1,44 @@
+﻿//  CSSS - CyberSecurity Scoring System
+//  Copyright(C) 2017  Jonathan Hart (stuajnht) <stuajnht@users.noreply.github.com>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using CSSSCheckerEngine;
+using CSSSConfig;
+using NLog;
+using System;
+
+namespace CSSS
+{
+    public class Prepare
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Creating an instance of the CSSS config class, to be
+        /// able to read and set values for it
+        /// </summary>
+        private static Config config = Config.GetCurrentConfig;
+        
+        /// <summary>
+        /// A reference to the <see cref="T:CSSSCheckerEngine.IssueFiles"/>
+        /// IssueFiles class, used when preparing the issue files
+        /// </summary>
+        private IssueFiles issueFiles = new IssueFiles();
+
+        public Prepare()
+        {
+        }
+    }
+}
