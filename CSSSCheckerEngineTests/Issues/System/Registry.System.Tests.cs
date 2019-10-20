81b427ddbd8f2b4430fcab3151cabe7840afbc17
Jonathan Hart <stuajnht@users.noreply.github.com>
2019-10-20 01:18:55 +0100

    Checked a supported OS is in use before trying to delete registry keys during CI tests
    Tests on Linux are failing with `TearDown Error : CSSSCheckerEngineTests.Issues.System.RegistryTests` due to an attempt to delete any registry keys being performed before a check is made to ensure it is supported by the operating system

--- CSSSCheckerEngineTests/Issues/System/Registry.System.Tests.cs
+++ CSSSCheckerEngineTests/Issues/System/Registry.System.Tests.cs
@@ -63,6 +63,11 @@ namespace CSSSCheckerEngineTests.Issues.System
         [OneTimeTearDown]
         protected void OneTimeTearDown()
         {
+            if (config.operatingSystemType != Config.OperatingSystemType.WinNT)
+            {
+                Assert.Ignore("Registry tests have been ignored due to this Operating System not having a registry");
+            }
+
             // `DeleteSubKeyTree` is used to save having to delete any subkeys
             // that are created, as `DeleteSubKey` doesn't do this
             Microsoft.Win32.Registry.CurrentUser.DeleteSubKeyTree(HiveRoot, false);
